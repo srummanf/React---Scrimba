956c803e75174f4e88880faf22e9f97e31d634af
Rumman <rummanfardeen4567@gmail.com>
2023-06-01 08:34:53 +0530

    Files added

---
+++ AirANB cards/src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
+    return (
+        <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../src/assets/images/${props.img}`} className="card--image" /> 
+            <div className="card--stats">
+                <img src="../src/assets/images/star.png" className="card--star" />
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
+            </div>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+        </div>
+    )
+}
+
+export default Card
